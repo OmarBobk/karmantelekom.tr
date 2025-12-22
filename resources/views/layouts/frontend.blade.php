3b8e32d6bc0165b9d3cc938760d2d762291a9677
Omar karman <karmanomar9696@gmail.com>
2025-12-22 21:35:49 +0300

    Add new git ignore rule and clean up frontend layout
    Added a rule to ignore JSON files in the analytics folder under storage/app. Also, adjusted formatting in the frontend Blade layout by adding spacing for better readability. No functional changes were introduced.

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -20,6 +20,7 @@
 
             gtag('config', 'G-YMHHC2JM4D');
         </script>
+
         <link rel="icon" type="image/png" href="{{ asset('assets/images/karmantelekom_logo.png') }}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
