15db05b613086d864b1128b5dd08a03ea69d161a
Omar karman <karmanomar9696@gmail.com>
2026-01-14 15:37:18 +0300

    Add Artisan command for database backup
    Introduced a new Artisan command `back-up-database` that dispatches the `BackUpDatabase` job, providing a convenient way to trigger database backups from the console. This enhances the application's backup capabilities and user experience by allowing manual backup initiation.

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Artisan::command('back-up-database', function () {
+    BackUpDatabase::dispatch();
+    $this->info('Database backup job has been dispatched!');
+})->purpose('Back up the database');
